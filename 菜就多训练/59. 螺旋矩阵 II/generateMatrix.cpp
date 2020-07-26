c51d247270b4c2aba48dbb781546c3e27ad853e2
ContinueZ <63264360@qq.com>
2020-07-26 15:01:27 +0800

    generateMatrix.cpp

--- 菜就多训练/59. 螺旋矩阵 II/generateMatrix.cpp
+++ 菜就多训练/59. 螺旋矩阵 II/generateMatrix.cpp
@@ -1,10 +1,63 @@
-#include "..\..\include.h"
+#include "..\include.h"
 
 class Solution {
 public:
 	vector<vector<int>> generateMatrix(int n) {
+		if (n == 0) return { {} };
+		if (n == 1) return { {1} };
 
+		vector<vector<int>> ans(n,vector<int>(n));
+		int l = 0;
+		int r = n - 1;
+		int b = n - 1;
+		int u = 0;
+		int k = 1;
+		while (l <= r&&u <= b) {
+			int i = u;
+			int j = l;
+			while (j <= r&&k<=n*n) {
+				ans[i][j] = k;
+				++k;
+				++j;
+			}
+			++u;
+			i = u;
+			j = r;
+			while (i <= b && k <= n * n) {
+				ans[i][j] = k;
+				++k;
+				++i;
+			}
+			--r;
+			j = r;
+			i = b;
+			while (j >= l && k <= n * n) {
+				ans[i][j] = k;
+				++k;
+				--j;
+			}
+			--b;
+			i = b;
+			j = l;
+			while (i >= u && k <= n * n) {
+				ans[i][j] = k;
+				++k;
+				--i;
+			}
+			++l;
+			j = l;
+			i = u;
+		}
+		return ans;
 	}
 };
 
+int main()
+{
+	int n = 3;
 
+	Solution s;
+	vector<vector<int>> ans = s.generateMatrix(n);
+
+	return 0;
+}
